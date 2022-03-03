0583ce599a79ef88f22fca01e4a6bcb6a9d7c05a
nikita.mekh <n.mekh@hutkigrosh.by>
2022-03-03 13:45:05 +0300

    CMSGATE-49: задание директории для хранения ключей

--- src/esas/cmsgate/security/CryptServiceImpl.php
+++ src/esas/cmsgate/security/CryptServiceImpl.php
@@ -6,24 +6,31 @@ namespace esas\cmsgate\security;
 
 use Defuse\Crypto\Crypto;
 use Defuse\Crypto\Key;
+use esas\cmsgate\Registry;
+use esas\cmsgate\utils\CMSGateException;
 
 class CryptServiceImpl extends CryptService
 {
+    private $keyDir;
     private $key;
 
-    public function __construct()
+    public function __construct($keyDir = null)
     {
+        if ($keyDir == null)
+            $keyDir = (dirname(__FILE__));
+        $this->keyDir = $keyDir;
         if (file_exists($this->keyFileName())) {
             $keyStr = file_get_contents($this->keyFileName());
             $this->key = Key::loadFromAsciiSafeString($keyStr);
         } else {
             $this->key = Key::createNewRandomKey();
-            file_put_contents($this->keyFileName(), $this->key->saveToAsciiSafeString());
+            if (!file_put_contents($this->keyFileName(), $this->key->saveToAsciiSafeString()))
+                throw new CMSGateException('Can not create key file', 'Incorrect config. Please connect to service support');
         }
     }
 
     private function keyFileName() {
-        return (dirname(__FILE__)) . '/key.bin';
+        return $this->keyDir . '/' . Registry::getRegistry()->getPaySystemName() . '-key.bin';
     }
 
     public function encrypt($data)
