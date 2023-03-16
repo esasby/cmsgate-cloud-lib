e8eb026ed9f42cfdbc521f99ba266f05917120c3
nikita.mekh <n.mekh@hutkigrosh.by>
2023-03-16 11:14:22 +0300

    CMSGATE-53: returnAbsolutePathOrRedirect

--- src/esas/cmsgate/view/RedirectService.php
+++ src/esas/cmsgate/view/RedirectService.php
@@ -1,11 +1,20 @@
 <?php
 namespace esas\cmsgate\view;
 
+use esas\cmsgate\utils\URLUtils;
+
 abstract class RedirectService
 {
     public static function redirect($location) {
         header('Location: '. $location);
         die();
+    }
+
+    public static function returnAbsolutePathOrRedirect($relativePath, $sendHeader = false) {
+        $location = URLUtils::getCurrentURLMainPart() . $relativePath;
+        if ($sendHeader)
+            self::redirect($location);
+        return $location;
     }
 
     public abstract function loginPage($sendHeader = false);
